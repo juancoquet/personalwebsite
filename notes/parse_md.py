6b723dfd96779e2e1a8acd6be843eb402c0d3da7
juancoquet <juancoquet@gmail.com>
2022-01-29 19:08:34 +0000

    remove h2 and h3 parse until code blocks are handled

--- notes/parse_md.py
+++ notes/parse_md.py
@@ -5,13 +5,3 @@ def parse_h1(markdown):
     h1 = re.compile(r'^#\s(.+)$', re.MULTILINE)
     replaced = h1.sub(r'<h1>\1</h1>', markdown)
     return replaced
-
-def parse_h2(markdown):
-    h2 = re.compile(r'^##\s(.+)$', re.MULTILINE)
-    replaced = h2.sub(r'<h2>\1</h2>', markdown)
-    return replaced
-
-def parse_h3(markdown):
-    h3 = re.compile(r'^###\s(.+)$', re.MULTILINE)
-    replaced = h3.sub(r'<h3>\1</h3>', markdown)
-    return replaced
--- notes/tests/test_parser.py
+++ notes/tests/test_parser.py
@@ -18,63 +18,20 @@ class ParserTest(TestCase):
         expected = 'preceding text\n<h1>Title being tested</h1>\nsome more text'
         self.assertEqual(result, expected)
 
-    def test_unintended_hash_symbol_doesnt_parse_to_h1(self):
-        text = 'the following # should not match'
-        result = parse_md.parse_h1(text)
-        expected = 'the following # should not match'
-        self.assertEqual(result, expected)
-
     def test_multiple_h1_parses(self):
         text = '# Title being tested\nsome random text\n# Title being tested2'
         result = parse_md.parse_h1(text)
         expected = '<h1>Title being tested</h1>\nsome random text\n<h1>Title being tested2</h1>'
         self.assertEqual(result, expected)
 
-    def test_parse_h2(self):
-        text = '## Title being tested'
-        result = parse_md.parse_h2(text)
-        expected = '<h2>Title being tested</h2>'
+    def test_unintended_hash_symbol_does_not_parse_to_h1(self):
+        text = 'the following # should not match'
+        result = parse_md.parse_h1(text)
+        expected = 'the following # should not match'
         self.assertEqual(result, expected)
 
-    def test_parse_h2_on_multiple_lines(self):
-        text = 'preceding text\n## Title being tested\nsome more text'
-        result = parse_md.parse_h2(text)
-        expected = 'preceding text\n<h2>Title being tested</h2>\nsome more text'
+    def test_h1_does_not_parse_inside_code_block(self):
+        text = '```\n# python comment\nmy_var = "hello world"\n```'
+        result = parse_md.parse_h1(text)
+        expected = '```\n# python comment\nmy_var = "hello world"\n```'
         self.assertEqual(result, expected)
-
-    def test_unintended_hash_symbol_doesnt_parse_to_h2(self):
-        text = 'the following ## should not match'
-        result = parse_md.parse_h2(text)
-        expected = 'the following ## should not match'
-        self.assertEqual(result, expected)
-
-    def test_multiple_h2_parses(self):
-        text = '## Title being tested\nsome random text\n## Title being tested2'
-        result = parse_md.parse_h2(text)
-        expected = '<h2>Title being tested</h2>\nsome random text\n<h2>Title being tested2</h2>'
-        self.assertEqual(result, expected)
-
-    def test_parse_h3(self):
-        text = '### Title being tested'
-        result = parse_md.parse_h3(text)
-        expected = '<h3>Title being tested</h3>'
-        self.assertEqual(result, expected)
-    
-    def test_parse_h3_on_multiple_lines(self):
-        text = 'preceding text\n### Title being tested\nsome more text'
-        result = parse_md.parse_h3(text)
-        expected = 'preceding text\n<h3>Title being tested</h3>\nsome more text'
-        self.assertEqual(result, expected)
-
-    def test_unintended_hash_symbol_doesnt_parse_to_h3(self):
-        text = 'the following ### should not match'
-        result = parse_md.parse_h3(text)
-        expected = 'the following ### should not match'
-        self.assertEqual(result, expected)
-
-    def test_multiple_h3_parses(self):
-        text = '### Title being tested\nsome random text\n### Title being tested2'
-        result = parse_md.parse_h3(text)
-        expected = '<h3>Title being tested</h3>\nsome random text\n<h3>Title being tested2</h3>'
-        self.assertEqual(result, expected)
-        
