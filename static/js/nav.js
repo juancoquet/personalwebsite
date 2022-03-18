4b351a9ab81a9d19c43df3f7e9855847f409dd65
juancoquet <juancoquet@gmail.com>
2022-03-18 01:10:53 +0000

    add mobile menu, no animation

--- static/js/nav.js
+++ static/js/nav.js
@@ -22,3 +22,33 @@ async function closeModal() {
     await sleep(0.5);
     modalAll.classList.add('hidden');
 }
+
+
+// Mobile Menu --------------------------------------------
+
+const menuBtn = document.querySelector('.nav__menu');
+const menuClose = document.querySelector('.nav__menu-close');
+const navItems = document.querySelectorAll('.nav__item');
+const menu = document.querySelector('.mobile-menu');
+const navRight = document.querySelector('.nav-items--right');
+
+menuBtn.addEventListener('click', openMenu);
+menuClose.addEventListener('click', closeMenu);
+
+function openMenu() {
+    navItems.forEach(item => {
+        menu.appendChild(item);
+        item.style.display = 'block';   
+    });
+    menuBtn.classList.toggle('hidden');
+    menuClose.classList.toggle('hidden');
+}
+
+function closeMenu() {
+    navItems.forEach(item => {
+        navRight.appendChild(item);
+        item.style.display = 'none';   
+    });
+    menuBtn.classList.toggle('hidden');
+    menuClose.classList.toggle('hidden');
+}
